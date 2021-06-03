d77f855d069d1b5859322396210e71b68c4c38a6
Keerthana1611 <84660994+Keerthana1611@users.noreply.github.com>
2021-06-03 19:14:43 +0530

    solution 1

--- wallis.c
+++ wallis.c
@@ -4,14 +4,16 @@
 #include <math.h>
 
 float wallis_pi(int);
-float wallis_pi(int m){
+
+float wallis_pi(int n)
+{
   float k=1.0;
   int i;
-  for(i=1;i<=m;i++){
+  for(i=1;i<=n;i++){
     k*=(float)(4.*i*i)/(4.*i*i-1);
   }
   return(k*2);
-  }
+}
 
 int main(void) {
   float pi;
@@ -31,5 +33,3 @@ int main(void) {
     }
   }
 }
-
-
